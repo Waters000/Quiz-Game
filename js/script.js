94188b43afb8713f9e08b6b23a25a4b3b27aedbf
Chris Waters <floridarealty001@gmail.com>
2022-01-13 10:20:07 -0500

    start quiz game coding

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+var btnStartEl = document.querySelector('#start-btn');
+
+// listed for button call
+
+btnStartEl.addEventListener('click', function() {
+    alert("button clicked");
+    console.log(btnStartEl);
+});
